6e13547b803836d3468626634b48b9a446e97f91
stefanRitter <stefan@stefanritter.com>
2014-02-14 13:05:00 +0100

    setup angular

--- mean/walking-skeleton/public/app/app.js
+++ mean/walking-skeleton/public/app/app.js
@@ -1,2 +1,13 @@
-angular.modul('', ['ngResource', 'ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute']);
 
+angular.module('app').config(function ($routeProvider, $locationProvider) {
+  //$locationProvider.html5mode(true);
+
+  $routeProvider
+    .when('/', {templateUrl: '/partials/main', controller: 'mainCtrl'});
+});
+
+// temp
+angular.module('app').controller('mainCtrl', function ($scope) {
+  $scope.myVar = 'Hello angular';
+});
