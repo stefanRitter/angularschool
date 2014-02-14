3682b84dc12f3fdf52fa708895284292c3517ef3
stefanRitter <stefan@stefanritter.com>
2014-02-14 16:27:24 +0100

    add mongoose

--- mean/walking-skeleton/server.js
+++ mean/walking-skeleton/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var stylus = require('stylus');
+var express = require('express'),
+    stylus = require('stylus'),
+    mongoose = require('mongoose');
+
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var port = 3030;
 
@@ -7,9 +9,9 @@ function compile (str, path) {
   return stylus(str).set('filename', path);
 }
 
+
+// setup server
 var app = express();
-
-
 app.configure(function() {
   app.set('views', __dirname + '/server/views');
   app.set('view engine', 'jade');
@@ -26,6 +28,18 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
+
+// setup datastore
+mongoose.connect('mongodb://localhost/app-test');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'db connection error...'));
+db.once('open', function() {
+  console.log('db connection opened');
+});
+
+
+// routes
 app.get('/partials/:partialPath', function (req, res) {
   res.render('partials/' + req.params.partialPath);
 });
