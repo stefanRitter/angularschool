df9ca468a16074f118e16c13b7caa172d1376ac1
stefanRitter <stefan@stefanritter.com>
2014-03-01 00:04:02 +0100

    filtering and sorting

--- mean/public/app/courses/appCourseListCtrl.js
+++ mean/public/app/courses/appCourseListCtrl.js
@@ -1,4 +1,8 @@
 
 angular.module('app').controller('appCourseListCtrl', function ($scope, appCourse) {
   $scope.courses = appCourse.query();
+
+  $scope.sortOptions = [{value: 'title', text: 'Sort by Title'},
+    {vaue: 'published', text: 'Sort by Publish Date'}];
+  $scope.sortOrder = $scope.sortOptions[0].value;
 });
