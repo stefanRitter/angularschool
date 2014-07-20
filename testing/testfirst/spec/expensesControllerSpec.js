126124c03b8acfed796051237f4a7511310c0306
stefanRitter <stefan@stefanritter.com>
2014-07-20 08:51:21 +0100

    setup controller spec

---
+++ testing/testfirst/spec/expensesControllerSpec.js
@@ -0,0 +1,18 @@
+
+describe('expenseController', function () {
+
+  var $controllerConstructor,
+      scope;
+
+  beforeEach(module('app'));
+  beforeEach(inject(function ($controller, $rootScope) {
+    $controllerConstructor = $controller;
+    scope = $rootScope.$new();
+  });
+
+  it('should have three expense items', function () {
+    var ctrl = $controllerConstructor('expenseController', {$scope: scope});
+    expect(ctrl.expenseItems.length).toBe(3);
+  });
+
+});
