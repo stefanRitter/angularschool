7f31da7134d4d5d793711f885b35259cabaf68e5
stefanRitter <stefan@stefanritter.com>
2014-07-27 15:53:19 +0100

    spy and fake

--- testing/testfirst/spec/persistingExpensesSpec.js
+++ testing/testfirst/spec/persistingExpensesSpec.js
@@ -12,4 +12,21 @@ describe('persisting expenses', function () {
       expect(spyCallback).toHaveBeenCalled();
     }));
   });
+
+  describe('spy on', function () {
+    
+    it('should spy on persistExpenses', inject(function (expensesDataService) {
+      var spy = spyOn(expensesDataService, 'persistExpenses');
+      expensesDataService.persistExpenses();
+      expect(spy).toHaveBeenCalled();
+    }));
+
+    it('should spy on persistExpenses and fake a reply', inject(function (expensesDataService) {
+      var spy = spyOn(expensesDataService, 'persistExpenses').and.callFake(function () {
+        return 3;
+      });
+      var numRecordsPersisted = expensesDataService.persistExpenses();
+      expect(numRecordsPersisted).toEqual(3);
+    }));
+  });
 });
